2d35cdd551c22e6b02a6d589cf5123449ce6a058
Alexandr_Li <Alexandr_li@epam.com>
2017-03-14 10:10:59 +0600

    add component lifecycle hooks logging

--- src/app/courses/course-list/course-list.component.ts
+++ src/app/courses/course-list/course-list.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    OnDestroy,
+    OnChanges,
+    DoCheck,
+    SimpleChanges,
+    AfterViewChecked,
+    AfterViewInit,
+    AfterContentChecked,
+    AfterContentInit
+} from '@angular/core';
 import { Course } from '../shared/course.model';
 
 @Component({
@@ -6,42 +17,27 @@ import { Course } from '../shared/course.model';
     templateUrl: 'course-list.component.html',
     styleUrls: ['./course-list.component.css']
 })
-export class CourseListComponent implements OnInit {
+export class CourseListComponent implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterViewChecked,
+    AfterViewInit,
+    AfterContentChecked,
+    AfterContentInit,
+    OnDestroy {
 
     public courses: Course[];
     public query: string = '';
 
     constructor() {
+        console.log('Contructor');
         this.courses = [];
     }
 
-    public ngOnInit() {
-        this.courses = [
-            {
-                id: 1,
-                title: 'Mock Course 1',
-                duration: 30,
-                creationDate: new Date(),
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris enim arcu, ultrices at feugiat a, mattis vitae urna.'
-            },
-            {
-                id: 2,
-                title: 'Mock Course 2',
-                duration: 70,
-                creationDate: new Date(),
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris enim arcu, ultrices at feugiat a, mattis vitae urna.'
-            },
-            {
-                id: 3,
-                title: 'Mock Course 3',
-                duration: 100,
-                creationDate: new Date(),
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris enim arcu, ultrices at feugiat a, mattis vitae urna.'
-            },
-        ];
-    }
 
-    public find() {
+
+    public find(): void {
         console.log(this.query);
     }
 
@@ -50,5 +46,68 @@ export class CourseListComponent implements OnInit {
         this.courses = this.courses.filter((course: Course) => {
             return course.id !== removeObj.id;
         });
+    }
+
+    public ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                title: 'Mock Course 1',
+                duration: 30,
+                creationDate: new Date(),
+                description:
+                `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris enim arcu, ultrices at feugiat a, mattis vitae urna.`
+            },
+            {
+                id: 2,
+                title: 'Mock Course 2',
+                duration: 70,
+                creationDate: new Date(),
+                description:
+                `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris enim arcu, ultrices at feugiat a, mattis vitae urna.`
+            },
+            {
+                id: 3,
+                title: 'Mock Course 3',
+                duration: 100,
+                creationDate: new Date(),
+                description:
+                `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris enim arcu, ultrices at feugiat a, mattis vitae urna.`
+            },
+        ];
+    }
+
+    public ngAfterContentInit(): void {
+        console.log('AfterContentInit');
+    }
+
+    public ngAfterContentChecked(): void {
+        console.log('AfterContentChecked');
+    }
+
+    public ngAfterViewInit(): void {
+        console.log('AfterViewInit');
+    }
+
+    public ngAfterViewChecked(): void {
+        console.log('AfterViewChecked');
+    }
+
+
+    public ngOnDestroy(): void {
+        console.log('OnDestroy');
+    }
+
+
+    public ngDoCheck(): void {
+        console.log('DoCheck');
+    }
+
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        console.log('OnChanges');
     }
 }
