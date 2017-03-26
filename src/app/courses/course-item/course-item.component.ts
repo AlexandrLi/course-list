c909bd4c203673080309cd4634aba5ee61e43e6b
Alexandr.li <muzikant1990@gmail.com>
2017-03-26 22:30:20 +0600

    Task4. Apply ChangeDetectionStrategy.OnPush

--- src/app/courses/course-item/course-item.component.ts
+++ src/app/courses/course-item/course-item.component.ts
@@ -3,18 +3,26 @@ import {
     OnInit,
     Input,
     Output,
-    EventEmitter
+    EventEmitter,
+    ChangeDetectionStrategy,
+    NgZone
 } from '@angular/core';
-import { Course } from '../shared/course.model';
+import { Course } from '../shared';
 
 @Component({
     selector: 'course-item',
     templateUrl: './course-item.component.html',
-    styleUrls: ['./course-item.component.css']
+    styleUrls: ['./course-item.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseItemComponent {
     @Input() public course: Course;
     @Output() public delete = new EventEmitter<number>();
+
+    constructor(private _ngZone: NgZone) {
+        _ngZone.onStable.subscribe(() => console.log('stable ' + new Date().toTimeString()));
+        _ngZone.onUnstable.subscribe(() => console.log('unstable ' + new Date().toTimeString()));
+    }
 
     public deleteCourse(): void {
         this.delete.emit(this.course.id);
