79d20a6b56a5c0b8113068757bb3bf103ff1be94
Alexandr_Li <Alexandr_li@epam.com>
2017-04-26 19:38:55 +0600

    import optimization

--- src/app/shared/loader/loader.component.ts
+++ src/app/shared/loader/loader.component.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/core';
 import { Subscription } from 'rxjs/Rx';
 
-import { LoaderService } from './loader.service';
+import { LoaderService } from './';
 
 @Component({
   selector: 'loader',
