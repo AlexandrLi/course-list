037fdb9d54825a13aeef2d5f6f7aba8adcc4449c
Alexandr.li <muzikant1990@gmail.com>
2017-05-01 18:49:15 +0600

    Add empty duration check in pipe

--- src/app/courses/shared/duration.pipe.ts
+++ src/app/courses/shared/duration.pipe.ts
@@ -6,10 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class DurationPipe implements PipeTransform {
   public transform(duration: number): string {
-    if (duration < 60) {
-      return `${duration} min`;
+    if (duration) {
+      // tslint:disable-next-line:max-line-length
+      return duration < 60 ? `${duration} min` : `${Math.trunc(duration / 60)} h ${duration % 60} min`;
     } else {
-      return `${Math.trunc(duration / 60)} h ${duration % 60} min`;
+      return '';
     }
   }
 }
