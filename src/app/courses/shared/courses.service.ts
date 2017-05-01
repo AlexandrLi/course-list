5d59bbed374b62ef9ce413d0366c818bff2bf6da
Alexandr.li <muzikant1990@gmail.com>
2017-05-01 18:45:57 +0600

    Add authors fetching from backend

--- src/app/courses/shared/courses.service.ts
+++ src/app/courses/shared/courses.service.ts
@@ -19,34 +19,7 @@ export class CoursesService {
   constructor(private http: AuthorizedHTTPService) {
   }
 
-  public getListByQuery(query: string, pageNumber: number, count: number) {
-    let requestOptions: RequestOptions = new RequestOptions();
-    let request: Request;
-    let urlParams: URLSearchParams = new URLSearchParams();
-    urlParams.set('q', query);
-    urlParams.set('_page', pageNumber.toString());
-    urlParams.set('_limit', count.toString());
-    requestOptions.url = `${this.baseUrl}/courses`;
-    requestOptions.method = RequestMethod.Get;
-    requestOptions.search = urlParams;
-
-    request = new Request(requestOptions);
-
-    return this.http.request(request)
-      .map((res: Response) => res.json())
-      .map((res: any[]) => res.map((item) => {
-        return {
-          id: item.id,
-          title: item.name,
-          duration: item.length,
-          date: new Date(item.date),
-          description: item.description,
-          topRated: item.isTopRated
-        };
-      }));
-  }
-
-  public getList(pageNumber: number, count: number, query: string): Observable<Course[]> {
+  public getCourseList(pageNumber: number, count: number, query: string): Observable<Course[]> {
     let twoWeeksAgo = (new Date()).getTime() - (14 * 24 * 60 * 60 * 1000);
     return this.http.get(`${this.baseUrl}/courses?page=${pageNumber}&limit=${count}&query=${query}`)
       .map((res: Response) => res.json())
@@ -61,6 +34,11 @@ export class CoursesService {
         };
       }))
       .map((courses: Course[]) => courses.filter((course) => course.date.getTime() >= twoWeeksAgo));
+  }
+
+  public getAuthorsList(): Observable<any[]> {
+    return this.http.get(`${this.baseUrl}/authors`)
+      .map((res: Response) => res.json());
   }
 
   public createCourse(): Observable<Course> {
