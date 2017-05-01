4edf9923c0912caca1f6db1b95c34d62af887b83
Alexandr.li <muzikant1990@gmail.com>
2017-05-01 18:48:57 +0600

    Implement add-course reactive form

--- src/app/courses/add-course/add-course.component.ts
+++ src/app/courses/add-course/add-course.component.ts
@@ -1,5 +1,9 @@
+import { Observable } from 'rxjs/Rx';
+import { CoursesService } from './../shared';
+import { DateValidators, DurationValidators, AuthorsValidators } from './../shared/validators';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'add-course',
@@ -8,10 +12,27 @@ import { Router } from '@angular/router';
 })
 
 export class AddCourseComponent {
+  public courseForm: FormGroup;
+  public authors: Observable<any[]>;
+  constructor(
+    private _router: Router,
+    private fb: FormBuilder,
+    public coursesService: CoursesService) {
+    this.authors = this.coursesService.getAuthorsList();
+    this.courseForm = this.fb.group({
+      title: ['', [Validators.required, Validators.maxLength(50)]],
+      description: ['', [Validators.required, Validators.maxLength(500)]],
+      date: ['', [Validators.required, DateValidators.shouldMatchFormat]],
+      duration: ['0', [Validators.required, DurationValidators.shouldContainNumbersOnly]],
+      authors: ['', AuthorsValidators.shouldCheckedAtLeastOne]
+    });
+  }
 
-  constructor(private _router: Router) { }
+  public submit(): void {
+    console.log(this.courseForm);
+  }
 
-  public cancel() {
+  public cancel(): void {
     this._router.navigate(['courses']);
   }
 }
